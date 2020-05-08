6ff65f00a9c40af169a7abb53d868af08e11f161
Loc Ngo <locngo@fortna.com>
2020-05-08 16:09:21 +0700

    Update docker base for fixing tzdata issue

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM yandex/clickhouse-server:19.16
+FROM yandex/clickhouse-server
 
 COPY init-hermesdb.sql /docker-entrypoint-initdb.d/init-hermesdb.sql
 COPY hermes_conf.xml /etc/clickhouse-server/config.d/hermes_conf.xml
--- init-hermesdb.sql
+++ init-hermesdb.sql
@@ -1,15 +1,16 @@
-
 -- create database and table
 CREATE DATABASE IF NOT EXISTS hermes;
 
-CREATE TABLE IF NOT EXISTS hermes.logs (
-  application String,
-  timestamp Int64,
-  date FixedString(8),
+CREATE TABLE IF NOT EXISTS hermes.logs
+(
+  id             Int64,
+  application    String,
+  timestamp      Int64,
+  date           FixedString(8),
   container_name String,
-  container_id String,
-  message String
+  container_id   String,
+  message        String
 )
-ENGINE = MergeTree()
-PARTITION BY (application, date)
-ORDER BY (application, timestamp);
+  ENGINE = MergeTree()
+    PARTITION BY (application, date)
+    ORDER BY (application, timestamp);
